66ed750966a9b0557b1fbc00ff4562ecf6dd3c32
pere <per@capia.no>
2024-11-25 20:00:04 +0100

    Update train.py

--- train.py
+++ train.py
@@ -376,6 +376,10 @@ def dpo_loss_fn(model, config, data, dropout_rng, params, reference_params, is_t
   loss = -jax.nn.log_sigmoid(scaled_ratios) * (1 - LABEL_SMOOTHING) - jax.nn.log_sigmoid(-scaled_ratios) * LABEL_SMOOTHING
   print("Loss shape before masking:", loss.shape)
   common_prefix_mask = jnp.cumsum(chosen_ids == rejected_ids) == 0
+
+  print("chosen_segmentation shape:", chosen_segmentation.shape)
+  print("rejected_segmentation shape:", rejected_segmentation.shape)
+  print("common_prefix_mask shape:", common_prefix_mask.shape)
   valid_mask = (chosen_segmentation != 0) & (rejected_segmentation != 0) & ~common_prefix_mask
   print("Valid mask shape:", valid_mask.shape)
   assert loss.shape == valid_mask.shape
